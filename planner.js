f0da03a4514162e3e1f989883f056a2e9b1a8dbb
azhang24 <andrewyzhang24@gmail.com>
2018-10-03 13:53:49 -0400

    Added dropdown functionality for planner

---
+++ planner.js
@@ -0,0 +1,8 @@
+$('#course-select1').dropdown();
+$('#requirement-select1').dropdown();
+$('#course-select2').dropdown();
+$('#requirement-select2').dropdown();
+$('#course-select3').dropdown();
+$('#requirement-select3').dropdown();
+$('#course-select4').dropdown();
+$('#requirement-select4').dropdown();
