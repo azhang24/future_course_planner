805f01a61ca91255bbf4decb96e3aaafbd239dc0
azhang24 <andrewyzhang24@gmail.com>
2018-10-22 14:24:21 -0400

    planner modifications

--- planner.js
+++ planner.js
@@ -1,8 +1,16 @@
-$('#course-select1').dropdown();
+/**$('#course-select1').dropdown();
 $('#requirement-select1').dropdown();
 $('#course-select2').dropdown();
 $('#requirement-select2').dropdown();
 $('#course-select3').dropdown();
 $('#requirement-select3').dropdown();
 $('#course-select4').dropdown();
-$('#requirement-select4').dropdown();
+$('#requirement-select4').dropdown();**/
+
+function addclass(){
+
+}
+
+function removeclass(){
+
+}
