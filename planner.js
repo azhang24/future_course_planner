9d0f2042238b44b9b4fa0f9a60c9f3d7d5334a35
azhang24 <andrewyzhang24@gmail.com>
2018-10-23 16:32:29 -0400

    implemented addclass and removeclass
    'Add Class' and 'Remove Class' buttons now work

--- planner.js
+++ planner.js
@@ -8,9 +8,66 @@ $('#course-select4').dropdown();
 $('#requirement-select4').dropdown();**/
 
 function addclass(){
+	/*var course_header = document.getElementsByTagName("th")[0]
+	var requirement_header = document.getElementsByTagName("th")[1]*/
 
+	var table_body = document.getElementById("table of classes").children[1]
+
+	var num_classes = table_body.childElementCount
+
+	var new_row = document.createElement("tr")
+
+	var new_course = document.createElement("td")
+
+	var dropdown1 = document.createElement("select")
+	dropdown1.className = "ui search selection dropdown"
+	dropdown1.id = "course-select" + (num_classes+1)
+	
+
+	var firstoption = document.createElement("option")
+	firstoption.innerHTML = "Course"
+
+	var firstoption2 = document.createElement("option")
+	firstoption2.innerHTML = "CS 2110"
+
+	dropdown1.appendChild(firstoption)
+	dropdown1.appendChild(firstoption2)
+
+	new_course.appendChild(dropdown1)
+
+
+	var new_requirement = document.createElement("td")
+
+	var dropdown2 = document.createElement("select")
+	dropdown2.className = "ui search selection dropdown"
+	dropdown2.id = "requirement-select" + (num_classes+1)
+
+	var secondoption = document.createElement("option")
+	secondoption.innerHTML = "Requirement"
+
+	dropdown2.appendChild(secondoption)
+
+	new_requirement.appendChild(dropdown2)
+
+	new_row.appendChild(new_course)
+	new_row.appendChild(new_requirement)
+
+	console.log(new_row)
+
+	table_body.appendChild(new_row)
+
+	$('#'+dropdown1.id).dropdown()
+	$('#'+dropdown2.id).dropdown()
 }
 
 function removeclass(){
+	var table_body = document.getElementById("table of classes").children[1]
+
+	if (table_body.childElementCount > 0){
+	
+		var class_to_remove = table_body.children[table_body.childElementCount - 1]
+
+		table_body.removeChild(class_to_remove)
+	}
 
 }
