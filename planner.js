4abf82c660f8a7641810de608a9abee806038813
azhang24 <andrewyzhang24@gmail.com>
2018-11-04 17:17:41 -0500

    added logo and data

--- planner.js
+++ planner.js
@@ -7,52 +7,35 @@ $('#requirement-select3').dropdown();
 $('#course-select4').dropdown();
 $('#requirement-select4').dropdown();**/
 
+function addnewoption(dropdown, optionName){
+	var option = document.createElement("option")
+	option.innerHTML = optionName
+	dropdown.appendChild(option)
+}
+
 function addclass(){
-	/*var course_header = document.getElementsByTagName("th")[0]
-	var requirement_header = document.getElementsByTagName("th")[1]*/
 
 	var table_body = document.getElementById("table of classes").children[1]
-
 	var num_classes = table_body.childElementCount
-
 	var new_row = document.createElement("tr")
-
+	
 	var new_course = document.createElement("td")
-
 	var dropdown1 = document.createElement("select")
 	dropdown1.className = "ui search selection dropdown"
 	dropdown1.id = "course-select" + (num_classes+1)
-	
-
-	var firstoption = document.createElement("option")
-	firstoption.innerHTML = "Course"
-
-	var firstoption2 = document.createElement("option")
-	firstoption2.innerHTML = "CS 2110"
-
-	dropdown1.appendChild(firstoption)
-	dropdown1.appendChild(firstoption2)
-
+	addnewoption(dropdown1, "Course")
+	addnewoption(dropdown1, "CS 2110")
 	new_course.appendChild(dropdown1)
 
-
 	var new_requirement = document.createElement("td")
-
 	var dropdown2 = document.createElement("select")
 	dropdown2.className = "ui search selection dropdown"
 	dropdown2.id = "requirement-select" + (num_classes+1)
-
-	var secondoption = document.createElement("option")
-	secondoption.innerHTML = "Requirement"
-
-	dropdown2.appendChild(secondoption)
-
+	addnewoption(dropdown2, "Requirement")
 	new_requirement.appendChild(dropdown2)
 
 	new_row.appendChild(new_course)
 	new_row.appendChild(new_requirement)
-
-	console.log(new_row)
 
 	table_body.appendChild(new_row)
 
@@ -72,7 +55,7 @@ function removeclass(){
 
 }
 
-function saveclass(){
+function saveclasses(){
 	
 }
 
