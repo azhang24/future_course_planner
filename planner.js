48c89c2d9493b5ad6ae15ac38f0637ee664e00cf
azhang24 <andrewyzhang24@gmail.com>
2018-11-09 17:18:49 -0500

    save courses in progress

--- planner.js
+++ planner.js
@@ -1,13 +1,6 @@
-/**$('#course-select1').dropdown();
-$('#requirement-select1').dropdown();
-$('#course-select2').dropdown();
-$('#requirement-select2').dropdown();
-$('#course-select3').dropdown();
-$('#requirement-select3').dropdown();
-$('#course-select4').dropdown();
-$('#requirement-select4').dropdown();**/
-
 $('#semesterdropdown').dropdown();
+
+var semestertoclasses = new Map();
 
 function addnewoption(dropdown, optionName){
 	var option = document.createElement("option")
@@ -17,7 +10,6 @@ function addnewoption(dropdown, optionName){
 }
 
 function addclass(){
-
 	var table_body = document.getElementById("table of classes").children[1]
 	var num_classes = table_body.childElementCount
 	var new_row = document.createElement("tr")
@@ -27,7 +19,10 @@ function addclass(){
 	dropdown1.className = "ui search selection dropdown"
 	dropdown1.id = "course-select" + (num_classes+1)
 	addnewoption(dropdown1, "Course")
-	addnewoption(dropdown1, "CS 2110")
+	addnewoption(dropdown1, "CS 2800")
+	addnewoption(dropdown1, "MATH 2940")
+	addnewoption(dropdown1, "CS 3110")
+	addnewoption(dropdown1, "CS 4780")
 	new_course.appendChild(dropdown1)
 
 	var new_requirement = document.createElement("td")
@@ -37,8 +32,9 @@ function addclass(){
 	addnewoption(dropdown2, "Requirement")
 	addnewoption(dropdown2, "Core Class")
 	addnewoption(dropdown2, "Major Course")
+	addnewoption(dropdown2, "Major Elective")
 	addnewoption(dropdown2, "External Specialization")
-	addnewoption(dropdown2, "Major Technical Elective")
+	addnewoption(dropdown2, "Technical Elective")
 	addnewoption(dropdown2, "Advisor-Approved Elective")
 	new_requirement.appendChild(dropdown2)
 
@@ -55,23 +51,54 @@ function removeclass(){
 	var table_body = document.getElementById("table of classes").children[1]
 
 	if (table_body.childElementCount > 0){
-	
 		var class_to_remove = table_body.children[table_body.childElementCount - 1]
-
 		table_body.removeChild(class_to_remove)
 	}
-
 }
 
 function saveclasses(){
 	var table_body = document.getElementById("table of classes").children[1]
 	var num_classes = table_body.childElementCount
-	var semester = document.getElementById("current_semester").innerText
-
+	var semester = document.getElementById("semesterdropdown").value
 	
+	var courses = table_body.children
 
+	var classtorequirement = new Map();
 
+	for (var i = 0; i < courses.length; i++){
+		var data = courses[i].children
+		classtorequirement.set(data[0].firstChild.children[3].innerHTML, 
+								data[1].firstChild.children[3].innerHTML)
+	}
+
+	semestertoclasses.set(semester, classtorequirement)
 	
+}
+
+function retreiveclasses(semester){
+	classes = semestertoclasses.get(semester)
+
+	var table_body = document.getElementById("table of classes").children[1]
+	while(table_body.firstChild){
+		table_body.removeChild(table_body.firstChild)
+	}
+
+	console.log(classes)
+	if(classes != undefined ){
+		var count = 1
+		for(var key of Array.from(classes.keys())){
+			addclass()
+			var course = document.getElementById("course-select" + (count.toString()))
+			course.value = key
+			course.options[course.options.selectedIndex].selected = true
+			console.log(course.options.selectedIndex)
+			var requirement = document.getElementById("requirement-select" + (count.toString()))
+			requirement.value = classes.get(key)
+			requirement.options[requirement.options.selectedIndex].selected = true
+			count++
+		}
+	}
+
 }
 
 function updatesemesterlist(){
@@ -117,5 +144,6 @@ function updatesemesterlist(){
 		addnewoption(semesterlist, "Spring" + " " + prevyear)
 	}
 	semesterlist.value = updatedsemester
+	retreiveclasses(updatedsemester)
 }
 
